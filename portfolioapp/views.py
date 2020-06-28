04839875850721ccb8a008f95b754e0c4be7c601
gresiandra <gresiandra@gmail.com>
2020-06-28 13:12:57 +0700

    consume api training

--- portfolioapp/views.py
+++ portfolioapp/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
 from .models import aboutme, experience, skill
+import urllib.request as ur
+import json
 
 # Create your views here.
 
 def index(request):
+
+    # longitude = []
+    # latitude = []
+
+    # response = ur.urlopen("https://api.wheretheiss.at/v1/satellites/25544")
+    # json_content = json.loads(response.read())
+    # longitude.append(json_content['longitude'])
+    # latitude.append(json_content['latitude'])
+
 
     about = aboutme.objects.all()
     myexperience = experience.objects.all()
@@ -12,6 +23,8 @@ def index(request):
     context = {
         'aboutme':about,
         'experience':myexperience,
-        'skill':myskill
+        'skill':myskill,
+        # 'lon':longitude,
+        # 'lat':latitude
     }
     return render(request, 'portfolioapp/index.html', context)
